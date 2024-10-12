9ea9f55cb868a6c818b4c62b28a74561fe95137b
neo773 <neo773@protonmail.com>
2024-10-12 17:43:37 +0530

    wip

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -31,6 +31,8 @@ class ToolsController < ApplicationController
         # Stock Portfolio Backtest
         :benchmark_stock, :investment_amount, :start_date, :end_date, { stocks: [], stock_allocations: [] },
         # Early Mortgage Payoff vs Investing Calculator
-        :loan_amount, :loan_term, :interest_rate, :elapsed_time, :additional_payment, :annual_investment_return
+        :loan_amount, :loan_term, :interest_rate, :additional_payment, :annual_investment_return, :elapsed_time,
+        :total_interest, :total_interest_with_extra_payments, :total_payments, :total_payments_with_extra,
+        :original_payoff_date, :new_payoff_date, :interest_savings, :months_saved, :investment_balance, :net_difference
     end
 end
--- app/presenters/tool/presenter/early_mortgage_payoff_vs_investing_calculator.rb
+++ app/presenters/tool/presenter/early_mortgage_payoff_vs_investing_calculator.rb
@@ -1,40 +1,44 @@
 class Tool::Presenter::EarlyMortgagePayoffVsInvestingCalculator < Tool::Presenter
-  attribute :loan_amount, :tool_float, default: 500000.0
+  attribute :loan_amount, :tool_float, default: 300000.0
   attribute :loan_term, :tool_integer, default: 30
-  attribute :interest_rate, :tool_percentage, default: 6.20
-  attribute :elapsed_time, :tool_integer, default: 0
+  attribute :interest_rate, :tool_percentage, default: 3.5
   attribute :additional_payment, :tool_float, default: 500.0
-  attribute :annual_investment_return, :tool_percentage, default: 4.0
+  attribute :annual_investment_return, :tool_percentage, default: 7.0
+  attribute :elapsed_time, :tool_integer, default: 0
 
   def blank?
     [ loan_amount, loan_term, interest_rate, additional_payment, annual_investment_return ].all?(&:zero?)
   end
 
   def interest_savings
-    original_interest - interest_with_extra_payments
+    (total_interest - total_interest_with_extra_payments).round(2)
+  end
+
+  def months_saved
+    (loan_term * 12 - months_to_payoff).round(0)
   end
 
   def new_payoff_date
-    Date.today + (loan_term * 12 - months_saved).months
+    Date.today + months_to_payoff.months
   end
 
   def investment_balance
-    plot_data.last[:contributed].round(2)
+    plot_data.last[:invested].round(2)
   end
 
   def net_difference
-    investment_balance - interest_savings
+    (investment_balance - interest_savings).round(2)
   end
 
   def legend_data
     {
-      interest: {
-        name: "Mortgage early payoff savings",
+      mortgage: {
+        name: "Mortgage Interest Savings",
         fillClass: "fill-green-500",
         strokeClass: "stroke-green-500"
       },
-      contributed: {
-        name: "Investment account balance",
+      invested: {
+        name: "Investment Growth",
         fillClass: "fill-blue-500",
         strokeClass: "stroke-blue-500"
       }
@@ -45,30 +49,43 @@ class Tool::Presenter::EarlyMortgagePayoffVsInvestingCalculator < Tool::Presente
     @plot_data ||= generate_plot_data
   end
 
-  private
+  def total_payments
+    (monthly_payment * loan_term * 12).round(2)
+  end
+
+
+  def original_payoff_date
+    Date.today + (loan_term * 12).months
+  end
+
+  def months_saved_formatted
+    years = months_saved / 12
+    remaining_months = months_saved % 12
+    "#{years} years #{remaining_months} months"
+  end
 
   def active_record
     @active_record ||= Tool.find_by!(slug: "early-mortgage-payoff-vs-investing-calculator")
-  end
-
-  def original_interest
-    monthly_payment * loan_term * 12 - loan_amount
-  end
-
-  def interest_with_extra_payments
-    total_payments_with_extra - loan_amount
   end
 
   def monthly_payment
     @monthly_payment ||= calculate_monthly_payment(loan_amount, interest_rate / 100 / 12, loan_term * 12)
   end
 
+  def total_interest
+    (monthly_payment * loan_term * 12 - loan_amount).round(2)
+  end
+
+  def total_interest_with_extra_payments
+    (total_payments_with_extra - loan_amount).round(2)
+  end
+
   def total_payments_with_extra
     calculate_total_payments_with_extra
   end
 
-  def months_saved
-    loan_term * 12 - calculate_months_to_payoff
+  def months_to_payoff
+    calculate_months_to_payoff
   end
 
   def calculate_monthly_payment(principal, monthly_rate, num_payments)
@@ -87,7 +104,7 @@ class Tool::Presenter::EarlyMortgagePayoffVsInvestingCalculator < Tool::Presente
       balance -= principal
     end
 
-    total_paid
+    total_paid.round(2)
   end
 
   def calculate_months_to_payoff
@@ -122,13 +139,13 @@ class Tool::Presenter::EarlyMortgagePayoffVsInvestingCalculator < Tool::Presente
       total_interest_paid += interest
 
       investment_balance = investment_balance * (1 + investment_monthly_rate) + additional_payment
-      mortgage_savings = original_interest - total_interest_paid
+      mortgage_savings = total_interest - total_interest_paid
 
       data << {
         year: year,
         date: Date.today + year.years,
-        interest: mortgage_savings,
-        contributed: investment_balance
+        mortgage: mortgage_savings,
+        invested: investment_balance
       }
 
       break if balance <= 0
